4f30332a958fad8a9d504e5b51720790fa9bfdf0
sahulhameed.s <sahulhameed.s@bthinq.com>
2023-08-11 11:55:52 +0530

    pointed db in appsetting & pointed share hostsing module

--- services/identity/host/Tasky.IdentityService.HttpApi.Host/IdentityServiceHttpApiHostModule.cs
+++ services/identity/host/Tasky.IdentityService.HttpApi.Host/IdentityServiceHttpApiHostModule.cs
@@ -1,56 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using IdentityModel;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.DataProtection;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Tasky.IdentityService.EntityFrameworkCore;
-using Tasky.IdentityService.MultiTenancy;
-using StackExchange.Redis;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tasky.IdentityService.EntityFrameworkCore;
+using Tasky.Shared.Hosting;
 using Volo.Abp;
-using Volo.Abp.AspNetCore.Mvc.UI.MultiTenancy;
-using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
-using Volo.Abp.AspNetCore.Serilog;
-using Volo.Abp.AuditLogging.EntityFrameworkCore;
-using Volo.Abp.Autofac;
 using Volo.Abp.Caching;
-using Volo.Abp.Caching.StackExchangeRedis;
-using Volo.Abp.EntityFrameworkCore;
-using Volo.Abp.EntityFrameworkCore.SqlServer;
-using Volo.Abp.Localization;
 using Volo.Abp.Modularity;
-using Volo.Abp.MultiTenancy;
-using Volo.Abp.PermissionManagement.EntityFrameworkCore;
-using Volo.Abp.Security.Claims;
-using Volo.Abp.SettingManagement.EntityFrameworkCore;
-using Volo.Abp.Swashbuckle;
-using Volo.Abp.TenantManagement.EntityFrameworkCore;
-using Volo.Abp.VirtualFileSystem;
 
 namespace Tasky.IdentityService;
 
 [DependsOn(
+    typeof(TaskyHostingModule),
     typeof(IdentityServiceApplicationModule),
     typeof(IdentityServiceEntityFrameworkCoreModule),
-    typeof(IdentityServiceHttpApiModule),
-    typeof(AbpAspNetCoreMvcUiMultiTenancyModule),
-    typeof(AbpAutofacModule),
-    typeof(AbpCachingStackExchangeRedisModule),
-    typeof(AbpEntityFrameworkCoreSqlServerModule),
-    typeof(AbpAuditLoggingEntityFrameworkCoreModule),
-    typeof(AbpPermissionManagementEntityFrameworkCoreModule),
-    typeof(AbpSettingManagementEntityFrameworkCoreModule),
-    typeof(AbpTenantManagementEntityFrameworkCoreModule),
-    typeof(AbpAspNetCoreSerilogModule),
-    typeof(AbpSwashbuckleModule)
-    )]
+    typeof(IdentityServiceHttpApiModule)
+)]
 public class IdentityServiceHttpApiHostModule : AbpModule
 {
 
@@ -59,26 +31,26 @@ public class IdentityServiceHttpApiHostModule : AbpModule
         var hostingEnvironment = context.Services.GetHostingEnvironment();
         var configuration = context.Services.GetConfiguration();
 
-        Configure<AbpDbContextOptions>(options =>
-        {
-            options.UseSqlServer();
-        });
+        //Configure<AbpDbContextOptions>(options =>
+        //{
+        //    options.UseSqlServer();
+        //});
 
-        Configure<AbpMultiTenancyOptions>(options =>
-        {
-            options.IsEnabled = MultiTenancyConsts.IsEnabled;
-        });
+        //Configure<AbpMultiTenancyOptions>(options =>
+        //{
+        //    options.IsEnabled = MultiTenancyConsts.IsEnabled;
+        //});
 
-        if (hostingEnvironment.IsDevelopment())
-        {
-            Configure<AbpVirtualFileSystemOptions>(options =>
-            {
-                options.FileSets.ReplaceEmbeddedByPhysical<IdentityServiceDomainSharedModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}src{0}Tasky.IdentityService.Domain.Shared", Path.DirectorySeparatorChar)));
-                options.FileSets.ReplaceEmbeddedByPhysical<IdentityServiceDomainModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}src{0}Tasky.IdentityService.Domain", Path.DirectorySeparatorChar)));
-                options.FileSets.ReplaceEmbeddedByPhysical<IdentityServiceApplicationContractsModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}src{0}Tasky.IdentityService.Application.Contracts", Path.DirectorySeparatorChar)));
-                options.FileSets.ReplaceEmbeddedByPhysical<IdentityServiceApplicationModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}src{0}Tasky.IdentityService.Application", Path.DirectorySeparatorChar)));
-            });
-        }
+        //if (hostingEnvironment.IsDevelopment())
+        //{
+        //    Configure<AbpVirtualFileSystemOptions>(options =>
+        //    {
+        //        options.FileSets.ReplaceEmbeddedByPhysical<IdentityServiceDomainSharedModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}src{0}Tasky.IdentityService.Domain.Shared", Path.DirectorySeparatorChar)));
+        //        options.FileSets.ReplaceEmbeddedByPhysical<IdentityServiceDomainModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}src{0}Tasky.IdentityService.Domain", Path.DirectorySeparatorChar)));
+        //        options.FileSets.ReplaceEmbeddedByPhysical<IdentityServiceApplicationContractsModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}src{0}Tasky.IdentityService.Application.Contracts", Path.DirectorySeparatorChar)));
+        //        options.FileSets.ReplaceEmbeddedByPhysical<IdentityServiceApplicationModule>(Path.Combine(hostingEnvironment.ContentRootPath, string.Format("..{0}..{0}src{0}Tasky.IdentityService.Application", Path.DirectorySeparatorChar)));
+        //    });
+        //}
 
         context.Services.AddAbpSwaggerGenWithOAuth(
             configuration["AuthServer:Authority"],
@@ -88,34 +60,34 @@ public class IdentityServiceHttpApiHostModule : AbpModule
             },
             options =>
             {
-                options.SwaggerDoc("v1", new OpenApiInfo {Title = "IdentityService API", Version = "v1"});
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "IdentityService API", Version = "v1" });
                 options.DocInclusionPredicate((docName, description) => true);
                 options.CustomSchemaIds(type => type.FullName);
             });
 
-        Configure<AbpLocalizationOptions>(options =>
-        {
-            options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
-            options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
-            options.Languages.Add(new LanguageInfo("en", "en", "English"));
-            options.Languages.Add(new LanguageInfo("en-GB", "en-GB", "English (UK)"));
-            options.Languages.Add(new LanguageInfo("fi", "fi", "Finnish"));
-            options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
-            options.Languages.Add(new LanguageInfo("hi", "hi", "Hindi", "in"));
-            options.Languages.Add(new LanguageInfo("is", "is", "Icelandic", "is"));
-            options.Languages.Add(new LanguageInfo("it", "it", "Italiano", "it"));
-            options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
-            options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
-            options.Languages.Add(new LanguageInfo("ro-RO", "ro-RO", "Română"));
-            options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
-            options.Languages.Add(new LanguageInfo("sk", "sk", "Slovak"));
-            options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
-            options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
-            options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
-            options.Languages.Add(new LanguageInfo("de-DE", "de-DE", "Deutsch"));
-            options.Languages.Add(new LanguageInfo("es", "es", "Español"));
-            options.Languages.Add(new LanguageInfo("el", "el", "Ελληνικά"));
-        });
+        //Configure<AbpLocalizationOptions>(options =>
+        //{
+        //    options.Languages.Add(new LanguageInfo("ar", "ar", "العربية"));
+        //    options.Languages.Add(new LanguageInfo("cs", "cs", "Čeština"));
+        //    options.Languages.Add(new LanguageInfo("en", "en", "English"));
+        //    options.Languages.Add(new LanguageInfo("en-GB", "en-GB", "English (UK)"));
+        //    options.Languages.Add(new LanguageInfo("fi", "fi", "Finnish"));
+        //    options.Languages.Add(new LanguageInfo("fr", "fr", "Français"));
+        //    options.Languages.Add(new LanguageInfo("hi", "hi", "Hindi", "in"));
+        //    options.Languages.Add(new LanguageInfo("is", "is", "Icelandic", "is"));
+        //    options.Languages.Add(new LanguageInfo("it", "it", "Italiano", "it"));
+        //    options.Languages.Add(new LanguageInfo("hu", "hu", "Magyar"));
+        //    options.Languages.Add(new LanguageInfo("pt-BR", "pt-BR", "Português"));
+        //    options.Languages.Add(new LanguageInfo("ro-RO", "ro-RO", "Română"));
+        //    options.Languages.Add(new LanguageInfo("ru", "ru", "Русский"));
+        //    options.Languages.Add(new LanguageInfo("sk", "sk", "Slovak"));
+        //    options.Languages.Add(new LanguageInfo("tr", "tr", "Türkçe"));
+        //    options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
+        //    options.Languages.Add(new LanguageInfo("zh-Hant", "zh-Hant", "繁體中文"));
+        //    options.Languages.Add(new LanguageInfo("de-DE", "de-DE", "Deutsch"));
+        //    options.Languages.Add(new LanguageInfo("es", "es", "Español"));
+        //    options.Languages.Add(new LanguageInfo("el", "el", "Ελληνικά"));
+        //});
 
         context.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
@@ -177,10 +149,10 @@ public class IdentityServiceHttpApiHostModule : AbpModule
         app.UseRouting();
         app.UseCors();
         app.UseAuthentication();
-        if (MultiTenancyConsts.IsEnabled)
-        {
-            app.UseMultiTenancy();
-        }
+        //if (MultiTenancyConsts.IsEnabled)
+        //{
+        app.UseMultiTenancy();
+        //}
         app.UseAbpRequestLocalization();
         app.UseAuthorization();
         app.UseSwagger();
